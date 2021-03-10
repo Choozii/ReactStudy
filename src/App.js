d5073e8e37d230d290cb39995c4392be89a299eb
choojiye <jiye_choo@tmax.co.kr>
2021-03-10 10:44:42 +0900

    [feat] 할 일 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useCallback} from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
-const App = () =>{
-  const [todos, setTodos] = useState([
-    {id:1, text:'잠자기', checked : true},
-    {id:2, text:'리액트 공부', checked : true},
-    {id:3, text:'7시 기상', checked : true},
-    {id:4, text:'출근하기', checked : false}
+
+const App = () => {
+  const [todos,setTodos] = useState([
+    {
+      id: 1,
+      text: '잠자기',
+      checked: true
+    }, {
+      id: 2,
+      text: '리액트 공부',
+      checked: true
+    }, {
+      id: 3,
+      text: '7시 기상',
+      checked: true
+    }, {
+      id: 4,
+      text: '출근하기',
+      checked: false
+    }
   ]);
+  const nextId = useRef(4);
+  
+  const onInsert = useCallback(text => {
+    const todo = {
+      id : nextId.current,
+      text,
+      checked : false
+    };
+    setTodos(todos.concat(todo));
+    nextId.current += 1;
+  }, [todos],);
+
   return <TodoTemplate>
-    <TodoInsert/>
+    <TodoInsert onInsert={onInsert}/>
     <TodoList todos={todos}/>
-    </TodoTemplate>
+  </TodoTemplate>
 }
 
 export default App;
